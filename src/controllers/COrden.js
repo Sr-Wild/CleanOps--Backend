e28b95b4bed2afe7f4373b84eaea89d9f5fa3334
Sr-Wild <wilderytg@gmail.com>
2022-06-30 15:37:10 -0400

    order controller

--- src/controllers/COrden.js
+++ src/controllers/COrden.js
@@ -1,18 +1,71 @@
-'use strict '
-const MOrden = require('../models/MOrden')
-const responses = require('../utils/responses')
+import { makeResponsesException, makeResponsesOkData } from '../utils/responses.js'
+import { Orden } from '../models/MOrden.js'
 
-async function getAllOrders (req, res) {
+export const getAllOrders = async (req, res) => {
   try {
-    const orders = await MOrder.findAll({
-        order: [['id', 'asc']]
+    const orders = await Orden.findAll({
+      order: [['id', 'asc']]
     })
-    responses.makeResponseOkData(res, orders, "Success")
+    makeResponsesOkData(res, orders, "Success")
   } catch (e) {
-    responses.makeResponsesException(res, e)
+    makeResponsesException(res, e)
   }
 }
 
-module.exports = {
-    getAllOrders
+export const getOrderById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const order = await Orden.findOne({
+      where: { id }
+    })
+    makeResponsesOkData(res, order, "Success")
+  } catch (e) {
+    makeResponsesException(res, e)
+  }
+}
+
+export const createOrder = async (req, res) => {
+  try {
+    const { trabajo, fechainicio,  fechacierre, estado } = req.body
+    const order = await Orden.create({
+     trabajo,
+     fechainicio,
+     fechacierre,
+     estado
+    })
+    makeResponsesOkData(res, order, "Success")
+  } catch (e) {
+    makeResponsesException(res, e)
+  }
+}
+
+export const updateOrder = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { trabajo, fechainicio,  fechacierre, estado } = req.body
+    const order = await Orden.update({
+     trabajo,
+     fechainicio,
+     fechacierre,
+     estado
+    },{
+      where: { id }
+    })
+    makeResponsesOkData(res, order, "Success")
+  } catch (e) {
+    makeResponsesException(res, e)
+  }
+}
+
+export const DeletOrderById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const order = await Orden.destroy({
+      where: { id }
+    })
+    makeResponsesOkData(res, order, "Success")
+
+  } catch (e) {
+    makeResponsesException(res, e)
+  }
 }
